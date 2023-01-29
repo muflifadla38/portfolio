824684f1af666316310cedd76d309836090e8e21
Mufli Fadla <muflifadlayt38@gmail.com>
2023-01-29 21:38:52 +0800

    setup layout with darkmode

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+const Layouts = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Mufly Fadla Portfolio | Web Developer"
+        />
+        <meta name="keywords" content="Web Developer, Portfolio, Resume, CV" />
+        <meta name="author" content="Mufly Fadla Syihab" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Mufli | Web Developer</title>
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+      </Head>
+      <div className="">
+        <main className="">{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layouts;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { Inter } from "@next/font/google";
+import { ThemeProvider } from "next-themes";
+import "@/styles/globals.css";
+import Layouts from "@/components/Layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const inter = Inter({ subsets: ["latin"] });
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider attribute="class">
+      <div className={inter.className}>
+        <Layouts>
+          <Component {...pageProps} />
+        </Layouts>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
