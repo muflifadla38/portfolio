333e573f5afb7fc2f515efcc149632a09f6d4743
Mufli Fadla <muflifadlayt38@gmail.com>
2023-04-13 03:44:56 +0800

    add education content

--- components/EducationSection.js
+++ components/EducationSection.js
@@ -1,26 +1,209 @@
-import React from "react";
+import { HiAcademicCap, HiUserGroup } from "react-icons/hi2";
 
 const EducationSection = () => {
   return (
     <>
       <div id="education" className="scroll-section">
         <div className="scroll-item">
-          {/* ADD edu content */}
-          <p className="text-center text-2xl font-bold">Education</p>
-        </div>
-      </div>
-      <div id="education" className="scroll-section">
-        <div className="scroll-item">
-          {/* ADD org content */}
-          <p className="text-center text-2xl font-bold">
-            Organization & Committee Experience
-          </p>
-        </div>
-      </div>
-      <div id="education" className="scroll-section">
-        <div className="scroll-item">
-          {/* ADD course content */}
-          <p className="text-center text-2xl font-bold">Courses</p>
+          <div className="flex flex-col">
+            <p className="text-center text-2xl font-bold uppercase mb-8">
+              Pendidikan & Organisasi
+            </p>
+            <div className="flex flex-row gap-x-14">
+              <div className="col">
+                <ol class="relative border-l border-slate-200 dark:border-slate-700">
+                  <li class="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                      S1 Geofisika
+                      <span class="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300 ml-3">
+                        Terbaru
+                      </span>
+                    </h3>
+                    <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
+                      Universitas Hasanuddin
+                    </p>
+                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                      Agustus 2016 - Agustus 2021
+                    </time>
+                  </li>
+                  <li class="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                      Front-End Web Developer
+                    </h3>
+                    <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
+                      Bootcamp Baparekraf Digital Talent
+                    </p>
+                    <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
+                      Dicoding
+                    </p>
+                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                      Juli 2020 - November 2020
+                    </time>
+                  </li>
+                  <li class="min-h-44 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                      Data Science & Machine Learning Dengan Python
+                    </h3>
+                    <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
+                      Workshop
+                    </p>
+                    <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
+                      Babastudio
+                    </p>
+                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                      Agustus 2020 - September 2020
+                    </time>
+                  </li>
+                </ol>
+              </div>
+              <div className="col">
+                <ol class="relative border-r border-slate-200 dark:border-slate-700">
+                  <li class="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiUserGroup class="fill-indigo-800 dark:fill-slate-300" />
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                      Himpunan Mahasiswa Fisika
+                    </h3>
+                    <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
+                      Universitas Hasanuddin
+                    </p>
+                    <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
+                      Anggota Divisi Pendidikan
+                    </p>
+                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                      2018-2019
+                    </time>
+                  </li>
+                  <li class="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiUserGroup class="fill-indigo-800 dark:fill-slate-300" />
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                      Society of Petroleum Engineers (SPE)
+                    </h3>
+                    <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
+                      Universitas Hasanuddin
+                    </p>
+                    <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
+                      Ketua Divisi Training
+                    </p>
+                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                      2019 - 2020
+                    </time>
+                  </li>
+                </ol>
+              </div>
+            </div>
+
+            {/* <div className="col">
+              <div className="w-60 relative">
+                <p className="text-xl font-bold text-center uppercase my-1">
+                  S1 Geofisika
+                </p>
+                <p className="text-sm text-center font-semibold text-slate-600 my-1">
+                  Universitas Hasanuddin
+                </p>
+                <p className="text-md text-center text-slate-400 uppercase my-1">
+                  2016 - 2021
+                </p>
+              </div>
+              <div className="w-60 relative my-24">
+                <p className="text-xl font-bold text-center uppercase my-1">
+                  Fullstack Web Development
+                </p>
+                <p className="text-sm text-center font-semibold text-slate-600 my-1">
+                  Bootcamp
+                </p>
+                <p className="text-xs text-center text-slate-500 uppercase my-1">
+                  Dicoding
+                </p>
+                <p className="text-md text-center text-slate-400 uppercase my-1">
+                  2016 - 2021
+                </p>
+              </div>
+              <div className="w-60 relative">
+                <p className="text-xl font-bold text-center uppercase my-1">
+                  Fullstack Web Development
+                </p>
+                <p className="text-sm text-center font-semibold text-slate-600 my-1">
+                  Bootcamp
+                </p>
+                <p className="text-xs text-center text-slate-500 uppercase my-1">
+                  Dicoding
+                </p>
+                <p className="text-md text-center text-slate-400 uppercase my-1">
+                  2016 - 2021
+                </p>
+              </div>
+            </div> */}
+            {/* timeline */}
+            {/* <div className="col">
+              <svg
+                width={16}
+                height={452}
+                viewBox="0 0 16 452"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-[15.06px] h-[451.06px]"
+                preserveAspectRatio="none"
+              >
+                <line
+                  x1="8.30914"
+                  y1="8.00143"
+                  x2="7.07025"
+                  y2={444}
+                  stroke="#A8DADC"
+                  stroke-width="1.005"
+                />
+                <circle cx="8.03" cy="7.03" r="6.53" stroke="#D08484" />
+                <circle cx="8.03" cy="7.03" r="4.03" fill="#E63946" />
+                <circle cx="8.03" cy="223.03" r="6.53" stroke="#D08484" />
+                <circle cx="8.03" cy="223.03" r="4.03" fill="#E63946" />
+                <circle cx="7.03" cy="444.03" r="6.53" stroke="#D08484" />
+                <circle cx="7.03" cy="444.03" r="4.03" fill="#E63946" />
+              </svg>
+            </div> */}
+            {/* <div className="col">
+              <div className="w-60 relative my-24">
+                <p className="text-xl font-bold text-center uppercase my-1">
+                  Fullstack Web Development
+                </p>
+                <p className="text-sm text-center font-semibold text-slate-600 my-1">
+                  Bootcamp
+                </p>
+                <p className="text-xs text-center text-slate-500 uppercase my-1">
+                  Dicoding
+                </p>
+                <p className="text-md text-center text-slate-400 uppercase my-1">
+                  2016 - 2021
+                </p>
+              </div>
+              <div className="w-60 relative my-24">
+                <p className="text-xl font-bold text-center uppercase my-1">
+                  Fullstack Web Development
+                </p>
+                <p className="text-sm text-center font-semibold text-slate-600 my-1">
+                  Bootcamp
+                </p>
+                <p className="text-xs text-center text-slate-500 uppercase my-1">
+                  Dicoding
+                </p>
+                <p className="text-md text-center text-slate-400 uppercase my-1">
+                  2016 - 2021
+                </p>
+              </div>
+            </div> */}
+          </div>
         </div>
       </div>
     </>
