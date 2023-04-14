1a110b4a830be5fea1c7282b328eb4c0749ca1e4
Mufli Fadla <muflifadlayt38@gmail.com>
2023-04-14 23:37:57 +0800

    change class to className

--- components/EducationSection.js
+++ components/EducationSection.js
@@ -11,29 +11,29 @@ const EducationSection = () => {
             </p>
             <div className="flex flex-row gap-x-14">
               <div className="col">
-                <ol class="relative border-l border-slate-200 dark:border-slate-700">
-                  <li class="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
-                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                <ol className="relative border-l border-slate-200 dark:border-slate-700">
+                  <li className="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap className="fill-indigo-800 dark:fill-slate-300" />
                     </span>
-                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
                       S1 Geofisika
-                      <span class="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300 ml-3">
+                      <span className="bg-indigo-100 text-indigo-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300 ml-3">
                         Terbaru
                       </span>
                     </h3>
                     <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
                       Universitas Hasanuddin
                     </p>
-                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                    <time className="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
                       Agustus 2016 - Agustus 2021
                     </time>
                   </li>
-                  <li class="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
-                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                  <li className="min-h-44 mb-10 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap className="fill-indigo-800 dark:fill-slate-300" />
                     </span>
-                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
                       Front-End Web Developer
                     </h3>
                     <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
@@ -42,15 +42,15 @@ const EducationSection = () => {
                     <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
                       Dicoding
                     </p>
-                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                    <time className="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
                       Juli 2020 - November 2020
                     </time>
                   </li>
-                  <li class="min-h-44 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
-                      <HiAcademicCap class="fill-indigo-800 dark:fill-slate-300" />
+                  <li className="min-h-44 ml-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -left-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiAcademicCap className="fill-indigo-800 dark:fill-slate-300" />
                     </span>
-                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
                       Data Science & Machine Learning Dengan Python
                     </h3>
                     <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
@@ -59,19 +59,19 @@ const EducationSection = () => {
                     <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
                       Babastudio
                     </p>
-                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                    <time className="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
                       Agustus 2020 - September 2020
                     </time>
                   </li>
                 </ol>
               </div>
               <div className="col">
-                <ol class="relative border-r border-slate-200 dark:border-slate-700">
-                  <li class="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
-                      <HiUserGroup class="fill-indigo-800 dark:fill-slate-300" />
+                <ol className="relative border-r border-slate-200 dark:border-slate-700">
+                  <li className="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiUserGroup className="fill-indigo-800 dark:fill-slate-300" />
                     </span>
-                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
                       Himpunan Mahasiswa Fisika
                     </h3>
                     <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
@@ -80,15 +80,15 @@ const EducationSection = () => {
                     <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
                       Anggota Divisi Pendidikan
                     </p>
-                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                    <time className="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
                       2018-2019
                     </time>
                   </li>
-                  <li class="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
-                      <HiUserGroup class="fill-indigo-800 dark:fill-slate-300" />
+                  <li className="min-h-44 mb-10 mr-8 block max-w-sm p-6 bg-white border border-slate-200 rounded-lg shadow-xl hover:bg-slate-100 dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-700">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-indigo-100 rounded-full -right-3 dark:ring-slate-900 dark:bg-indigo-900">
+                      <HiUserGroup className="fill-indigo-800 dark:fill-slate-300" />
                     </span>
-                    <h3 class="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-slate-900 dark:text-white">
                       Society of Petroleum Engineers (SPE)
                     </h3>
                     <p className="text-sm font-normal text-slate-500 dark:text-slate-400 mb-2">
@@ -97,7 +97,7 @@ const EducationSection = () => {
                     <p className="text-sm font-normal text-slate-400 dark:text-slate-500 mb-2">
                       Ketua Divisi Training
                     </p>
-                    <time class="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                    <time className="block mb-2 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
                       2019 - 2020
                     </time>
                   </li>
