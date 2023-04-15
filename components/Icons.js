54c8ba801f1afb9073490d6b1cdf363fa1205a60
Mufli Fadla <muflifadlayt38@gmail.com>
2023-04-15 20:46:02 +0800

    add svg icon

---
+++ components/Icons.js
@@ -0,0 +1,57 @@
+import AdobeIndesignIcon from "./Icons/AdobeindesignIcon";
+import AdobeXdIcon from "./Icons/AdobexdIcon";
+import BootstrapIcon from "./Icons/BootstrapIcon";
+import Css3Icon from "./Icons/Css3Icon";
+import ExpressJsIcon from "./Icons/ExpressjsIcon";
+import FigmaIcon from "./Icons/FigmaIcon";
+import FirebaseIcon from "./Icons/FirebaseIcon";
+import GitIcon from "./Icons/GitIcon";
+import HerokuIcon from "./Icons/HerokuIcon";
+import Html5Icon from "./Icons/Html5Icon";
+import JavascriptIcon from "./Icons/JavascriptIcon";
+import JqueryIcon from "./Icons/JqueryIcon";
+import LaravelIcon from "./Icons/LaravelIcon";
+import LinuxIcon from "./Icons/LinuxIcon";
+import MongodbIcon from "./Icons/MongodbIcon";
+import MysqlIcon from "./Icons/MysqlIcon";
+import NetworkIcon from "./Icons/NetworkIcon";
+import NextJsIcon from "./Icons/NextJsIcon";
+import NodeJsIcon from "./Icons/NodeJsIcon";
+import PhotoshopIcon from "./Icons/PhotoshopIcon";
+import PhpIcon from "./Icons/PhpIcon";
+import PythonIcon from "./Icons/PythonIcon";
+import ReactIcon from "./Icons/ReactIcon";
+import ReduxIcon from "./Icons/ReduxIcon";
+import RestAPIIcon from "./Icons/RestAPIIcon";
+import TailwindIcon from "./Icons/TailwindIcon";
+import WordpressIcon from "./Icons/WordpressIcon";
+
+export {
+  AdobeXdIcon,
+  AdobeIndesignIcon,
+  BootstrapIcon,
+  Css3Icon,
+  ExpressJsIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  GitIcon,
+  HerokuIcon,
+  Html5Icon,
+  JavascriptIcon,
+  JqueryIcon,
+  LaravelIcon,
+  LinuxIcon,
+  MongodbIcon,
+  MysqlIcon,
+  NetworkIcon,
+  NextJsIcon,
+  NodeJsIcon,
+  PhotoshopIcon,
+  PhpIcon,
+  PythonIcon,
+  ReactIcon,
+  ReduxIcon,
+  RestAPIIcon,
+  TailwindIcon,
+  WordpressIcon,
+};
---
+++ components/Icons/RestAPIIcon.js
@@ -0,0 +1,32 @@
+const RestAPIIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000"
+      baseProfile="tiny"
+      overflow="visible"
+      version="1.1"
+      viewBox="0 0 24 24"
+      xmlSpace="preserve"
+    >
+      <g>
+        <path fill="none" d="M0 0H24V24H0z"></path>
+        <g fillRule="evenodd" transform="translate(1 8)">
+          <path
+            fill="#5C85DE"
+            d="M2-1.9C.9-1.9-.3-.8-.3.3V10H2V5.5h2.2V10h2.2V.3c0-1.1-1.1-2.2-2.3-2.2H2zm0 5.1v-3h2.2v3H2zM10.3-2C9.1-2 8-.9 8 .2V10h2.2V5.5h2.2c1.1 0 2.3-1.1 2.3-2.2v-3c0-1.1-1.1-2.2-2.3-2.2h-2.1V-2zm-.1 5.2v-3h2.2v3h-2.2z"
+          ></path>
+          <path
+            fill="#5C85DE"
+            d="M18.5 0.3L18.5 7.8 16.2 7.8 16.2 10 23 10 23 7.8 20.8 7.8 20.8 0.3 23 0.3 23 -1.9 16.2 -1.9 16.2 0.3z"
+          ></path>
+          <path fill="#3367D6" d="M2 5.5L2 3.2 3.5 3.2z"></path>
+          <path fill="#3367D6" d="M10.2 5.5L10.2 3.2 11.5 3.2z"></path>
+          <path fill="#3367D6" d="M18.5 1.8L18.5 1.8 18.5 0.3 20.8 0.3z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default RestAPIIcon;
