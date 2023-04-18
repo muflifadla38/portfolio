abb96e4e8bb50b39fc6bad437914fccf6e09ed8b
Mufli Fadla <muflifadlayt38@gmail.com>
2023-04-18 14:20:25 +0800

    fix icon

--- components/Icons/AdobeIndesignIcon.js
+++ components/Icons/AdobeIndesignIcon.js
@@ -2,8 +2,6 @@ const AdobeIndesignIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="200"
-      height="200"
       fill="#000"
       version="1.1"
       viewBox="0 0 52 52"
